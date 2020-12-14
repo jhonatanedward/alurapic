4eef9bf2f4601e72e9b5b20076fe7f5f6a144d43
jhonatan edward <jhonatanedward@MacBook-Air-de-jhonatan.local>
2020-12-14 00:07:42 -0300

    Usuario logado e protecao de rotas

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,10 +6,14 @@ import {RouterModule, Routes} from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes : Routes = [
     {
-        path: '', component: SignInComponent
+        path: '', component: SignInComponent,
+        canActivate: [
+            AuthGuard
+        ]
     },
     {
         path: 'user/:userName', 
